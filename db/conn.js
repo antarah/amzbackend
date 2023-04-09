fef84ea6dfdef9a70e6ff3b06c552b0fe146b0f6
antarah <howalantara@gmail.com>
2023-04-10 01:48:13 +0530

    change4

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const DB= process.env.DATABASE;
+const DB= "mongodb+srv://antarah:37150411@cluster0.4eaflc1.mongodb.net/Amazonweb?retryWrites=true&w=majority";
 //console.log(DB)
 
 mongoose.connect(DB,{
